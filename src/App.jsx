8fd9d3cdece8d37612a95d75a2a441452fe8e953
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-07-06 18:29:05 -0600

    Refactor App component to manage new item state and update form structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
-  return <form className="new-item-form">
+  const [newItem, setNewItem] = useState("");
 
-  </form>
-} 
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To-do List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
