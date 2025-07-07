f226620d0acfb723b3ce579b873bbe4184ef9197
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-07-06 19:54:32 -0600

    Add TodoList and TodoItem components; implement localStorage for todo management

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NewTodoForm } from "./NewTodoForm.jsx";
+import { TodoList } from "./TodoList.jsx";
 import "./styles.css";
 
 export default function App() {
-  
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
 
- function addTodo(title) {
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
     setTodos((currentTodos) => {
       return [
         ...currentTodos,
         { id: crypto.randomUUID(), title: title, completed: false },
       ];
     });
-  } 
+  }
 
   function toggleTodo(id, completed) {
     setTodos((currentTodos) => {
@@ -36,30 +45,7 @@ export default function App() {
     <>
       <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">To-do List</h1>
-      <ul className="list">
-        {todos.length === 0 && "No items in the list"}
-
-        {todos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
-                />
-                {todo.title}
-              </label>
-              <button
-                onClick={() => deleteTodo(todo.id)}
-                className="btn btn-danger"
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   );
 }
